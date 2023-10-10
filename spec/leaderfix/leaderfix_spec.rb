1cbfba40ded45f4dd996b416cccec99cf4f330c8
Bess Sadler <bess.sadler@princeton.edu>
2023-10-10 17:48:49 -0400

    Ignore namespaces in marc xml

--- spec/leaderfix/leaderfix_spec.rb
+++ spec/leaderfix/leaderfix_spec.rb
@@ -1,15 +1,15 @@
-require "nokogiri"
-require "marc"
-require "byebug"
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'marc'
+require 'byebug'
 
 RSpec.describe 'fake test to set up CI' do
-  let(:marcfile) {"#{Dir.getwd}/spec/fixtures/malformed_leaders/marc_with_malformed_leaders.xml"}
-    it 'returns true' do
-      
-      reader = MARC::XMLReader.new(marcfile, parser: "magic")
-      byebug
-      reader.each do |record|
-        byebug
-      end
+  let(:marcfile) { "#{Dir.getwd}/spec/fixtures/malformed_leaders/marc_with_malformed_leaders.xml" }
+  it 'reads from a file of marc xml' do
+    reader = MARC::XMLReader.new(marcfile, parser: :nokogiri, ignore_namespace: true)
+    reader.each do |record|
+      puts record.to_marc
     end
   end
+end
