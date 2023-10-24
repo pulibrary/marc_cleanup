f6e69af05bf0f26ce915d73d726d36f824c998a4
Tyler Wade <tw8766@princeton.edu>
2023-10-24 11:17:25 -0400

    made rspec that tests for a valid leader

--- spec/leaderfix/leaderfix_spec.rb
+++ spec/leaderfix/leaderfix_spec.rb
@@ -1,15 +1,13 @@
 require "nokogiri"
 require "marc"
 require "byebug"
+require "marc_cleanup"
 
-RSpec.describe 'fake test to set up CI' do
+RSpec.describe 'test malformed leaders' do
   let(:marcfile) {"#{Dir.getwd}/spec/fixtures/malformed_leaders/marc_with_malformed_leaders.xml"}
-    it 'returns true' do
-      
-      reader = MARC::XMLReader.new(marcfile, parser: "magic")
-      byebug
-      reader.each do |record|
-        byebug
-      end
+    it 'knows that a record with a valid leader is valid' do  
+      reader = MARC::XMLReader.new(marcfile, parser: "magic", ignore_namespace: true)
+      record_with_valid_leader = reader.first
+      expect(MarcCleanup.leader_errors?(record_with_valid_leader)).to eq false
     end
   end
