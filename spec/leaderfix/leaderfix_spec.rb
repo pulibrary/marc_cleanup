2df6f60b4c4d830ab0fcf772c6e3ce7d8ef6c183
Tyler Wade <tw8766@princeton.edu>
2023-10-25 11:29:11 -0400

    first valid leader test for individual xml record

--- spec/leaderfix/leaderfix_spec.rb
+++ spec/leaderfix/leaderfix_spec.rb
@@ -4,7 +4,7 @@ require "byebug"
 require "marc_cleanup"
 
 RSpec.describe 'test malformed leaders' do
-  let(:marcfile) {"#{Dir.getwd}/spec/fixtures/malformed_leaders/marc_with_malformed_leaders.xml"}
+  let(:marcfile) {"#{Dir.getwd}/spec/fixtures/malformed_leaders/marc_with_valid_leader.xml"}
     it 'knows that a record with a valid leader is valid' do  
       reader = MARC::XMLReader.new(marcfile, parser: "magic", ignore_namespace: true)
       record_with_valid_leader = reader.first
