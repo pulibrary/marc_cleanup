a9d3cb5b06f01ae6fb9bcbe7af3569b5ca5b8552
Beck Davis <bd4538@princeton.edu>
2023-11-02 14:49:24 -0400

    created a duplicate fields spec file

---
+++ spec/global_fixes/remove_duplicate_fields_spec.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+require 'marc'
+require 'byebug'
+require 'marc_cleanup'
+
+Rspec.describe 'remove_duplicate_fields' do
+  describe 'record with duplicate fields' do
+    it 'removes duplicate 500 field' do
+    end
+  end
+end 
