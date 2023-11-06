cf72df1abb0094f1c54227af836b415f67b62c65
Beck Davis <bd4538@princeton.edu>
2023-11-06 15:15:12 -0500

    created test for remove_duplicate_fields method
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

--- spec/global_fixes/remove_duplicate_fields_spec.rb
+++ spec/global_fixes/remove_duplicate_fields_spec.rb
@@ -3,9 +3,16 @@ require 'marc'
 require 'byebug'
 require 'marc_cleanup'
 
-Rspec.describe 'remove_duplicate_fields' do
+
+RSpec.describe 'remove_duplicate_fields' do
   describe 'record with duplicate fields' do
+    let(:marcfile) { "#{Dir.getwd}/spec/fixtures/global_errors/marc_with_duplicate_fields.xml" }
+    let(:reader) { MARC::XMLReader.new(marcfile, parser: 'magic') }
+    let(:record_with_duplicate_fields) { reader.first }
     it 'removes duplicate 500 field' do
+      corrected_record = MarcCleanup.remove_duplicate_fields(record_with_duplicate_fields)
+      f500 = corrected_record.fields('500').select { |field| field['a'] == 'This is a note' }
+      expect(f500.size).to eq 1
     end
   end
 end 
