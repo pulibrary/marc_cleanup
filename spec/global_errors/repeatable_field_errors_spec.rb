29b4aa39b5fb69d1173616e84fe8058329c5577c
Beck Davis <136358877+Beck-Davis@users.noreply.github.com>
2023-11-08 10:39:03 -0500

    new test for repeatable_field_errors method (#58)
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

---
+++ spec/global_errors/repeatable_field_errors_spec.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'marc'
+require 'byebug'
+require 'marc_cleanup'
+
+RSpec.describe 'repeatable_field_errors?' do
+  describe 'repeated non-repeatable field' do
+    let(:marcfile) { "#{Dir.getwd}/spec/fixtures/global_errors/marc_with_multiple_001.xml" }
+    let(:reader) { MARC::XMLReader.new(marcfile, parser: 'magic') }
+    let(:record_with_repeatable_field_errors) { reader.first }
+    it 'finds repeatable field errors in the record' do
+      expect(MarcCleanup.repeatable_field_errors?(record_with_repeatable_field_errors)).to eq true
+    end
+  end
+end
