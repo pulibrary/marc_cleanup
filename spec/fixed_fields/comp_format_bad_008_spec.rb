a4e6a02a90b3f45a83967c1688de8bf06720bc58
Beck Davis <bd4538@princeton.edu>
2023-11-22 10:04:35 -0500

    applied changes from comments by @mzelesky

--- spec/fixed_fields/comp_format_bad_008_spec.rb
+++ spec/fixed_fields/comp_format_bad_008_spec.rb
@@ -3,12 +3,12 @@ require 'marc'
 require 'byebug'
 require 'marc_cleanup'
 
-RSpec.describe 'test  008 ' do
+RSpec.describe 'test bad_008?' do
   describe 'bad computer format 008' do
     let(:marcfile) {"#{Dir.getwd}/spec/fixtures/fixed_fields/marc_with_bad_008.xml"}
     let(:reader) { MARC::XMLReader.new(marcfile, parser: 'magic') }
     let(:record_with_bad_008) { reader.first }
-    it 'knows that a record with bad 008 is invalid' do
+    it 'knows that a record with bad computer 008 is invalid' do
       expect(MarcCleanup.bad_008?(record_with_bad_008)).to eq true
     end
   end
