3709bbfd091df97262e52677cd2d5f4c60687809
Beck Davis <bd4538@princeton.edu>
2023-11-22 12:59:29 -0500

    Test and fixture for book_008 method

---
+++ spec/fixed_fields/book_bad_008_spec.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'marc'
+require 'byebug'
+require 'marc_cleanup'
+
+RSpec.describe 'test bad_008?' do
+  describe 'bad book format 008' do
+    let(:marcfile) {"#{Dir.getwd}/spec/fixtures/fixed_fields/marc_with_bad_book_008.xml"}
+    let(:reader) { MARC::XMLReader.new(marcfile, parser: 'magic') }
+    let(:record_with_bad_008) { reader.first }
+    it 'knows that a record with bad book 008 is invalid' do
+      expect(MarcCleanup.bad_008?(record_with_bad_008)).to eq true
+    end
+  end
+end
