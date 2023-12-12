e7ab710b0b8275e542e7cd3d97000adb125b3eee
Beck Davis <bd4538@princeton.edu>
2023-12-12 16:26:14 -0500

    Corrected leader for mix_mat'

--- spec/fixed_fields/bad_008_spec.rb
+++ spec/fixed_fields/bad_008_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe 'bad_008?' do
         { '008' => '230519s1996    njua   u      000 0 eng d' }
       ]
     end
-    let(:leader) { '01104nga a2200289 i 4500' }
+    let(:leader) { '01104npa a2200289 i 4500' }
     let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
     it 'knows that a record with bad mix_mat 008 is invalid' do
       expect(MarcCleanup.bad_008?(record)).to eq true
