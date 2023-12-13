a34c7c7b436c6e34d63d6de9719a20bf2152d4d6
Tyler Wade <tw8766@princeton.edu>
2023-12-13 13:48:57 -0500

    new format for 0xx testing

--- spec/variable_fields/0xx_spec.rb
+++ spec/variable_fields/0xx_spec.rb
@@ -4,12 +4,17 @@ require 'byebug'
 require 'marc_cleanup'
 
 RSpec.describe 'test 0xx methods' do
-  describe '041 methods in variable fields' do
-    let(:marcfile) {"#{Dir.getwd}/spec/fixtures/variable_fields/marc_with_041.xml"}
-    let(:reader) { MARC::XMLReader.new(marcfile, parser: 'magic', ignore_namespace: true) }
-    let(:record_with_valid_041) { reader.first }
+  describe 'f041_errors?' do
+    let(:fields) do
+      [
+        { '041' => { 'indicator1' => ' ',
+                     'indicator2' => ' ',
+                     'subfields' => [{ 'a' => 'eng' }] } }
+      ]
+    end
+    let(:record) { MARC::Record.new_from_hash('fields' => fields) }
     it 'has no errors for a valid 041' do
-      expect(MarcCleanup.f041_errors?(record_with_valid_041)).to eq false
+      expect(MarcCleanup.f041_errors?(record)).to eq false
     end
   end
 end
