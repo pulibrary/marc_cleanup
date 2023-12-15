3e13f827cb90ff690e4ecec3455654445d609ca1
Beck Davis <bd4538@princeton.edu>
2023-12-15 09:52:59 -0500

    Additional spec tests for fix_008

--- spec/fixed_fields/008_spec.rb
+++ spec/fixed_fields/008_spec.rb
@@ -3,89 +3,107 @@ require 'marc'
 require 'byebug'
 require 'marc_cleanup'
 
-RSpec.describe 'bad_008?' do
-  describe 'bad book format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
+RSpec.describe 'field 008 methods'
+  describe 'bad_008?' do
+    describe 'bad book format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104naa a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad book 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
     end
-    let(:leader) { '01104naa a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad book 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
+    describe 'bad computer format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104nma a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad computer 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+    end
+    describe 'bad map format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104nea a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad map 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+    end
+    describe 'bad music format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104nca a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad music 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+    end
+    describe 'bad continuing resource format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104nab a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad continuing resource 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+    end
+    describe 'bad visual format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104nga a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad visual 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+    end
+    describe 'bad mix_mat format 008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104npa a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows that a record with bad mix_mat 008 is invalid' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
     end
   end
-  describe 'bad computer format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
+  describe 'fix_008' do
+    describe 'fix_book_008' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua   u      000 0 eng d' }
+        ]
+      end
+      let(:leader) { '01104naa a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'corrects a bad book format 008' do
+        fix_book_008(field)
+        expect(record['008']).to eq '230519s1996    njua   u      000 0 eng d'
+      end
     end
-    let(:leader) { '01104nma a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad computer 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
+
   end
-  describe 'bad map format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
-    end
-    let(:leader) { '01104nea a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad map 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
-  end
-  describe 'bad music format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
-    end
-    let(:leader) { '01104nca a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad music 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
-  end
-  describe 'bad continuing resource format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
-    end
-    let(:leader) { '01104nab a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad continuing resource 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
-  end
-  describe 'bad visual format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
-    end
-    let(:leader) { '01104nga a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad visual 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
-  end
-  describe 'bad mix_mat format 008' do
-    let(:fields) do
-      [
-        { '008' => '230519s1996    njua   u      000 0 eng d' }
-      ]
-    end
-    let(:leader) { '01104npa a2200289 i 4500' }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-    it 'knows that a record with bad mix_mat 008 is invalid' do
-      expect(MarcCleanup.bad_008?(record)).to eq true
-    end
-  end
-end
+end  
