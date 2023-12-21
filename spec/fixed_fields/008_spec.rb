829604b10089a8fdbe201642d1a7fe867b2b7d42
Beck Davis <bd4538@princeton.edu>
2023-12-21 14:08:02 -0500

    readded bad 008 length test with altered leader

--- spec/fixed_fields/008_spec.rb
+++ spec/fixed_fields/008_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'field 008 methods' do
     describe 'bad book format 008' do
       let(:fields) do
         [
-          { '008' => '230519s1996    njuax         000 0 eng d ' }
+          { '008' => '230519s1996    njuax         000 0 eng d' }
         ]
       end
       let(:leader) { '01104naa a2200289 i 4500' }
@@ -89,6 +89,18 @@ RSpec.describe 'field 008 methods' do
         expect(MarcCleanup.bad_008?(record)).to eq true
       end
     end
+    describe 'bad 008 length' do
+      let(:fields) do
+        [
+          { '008' => '230519s1996    njua         000 0 eng a' }
+        ]
+      end
+      let(:leader) { '01104n a a2200289 i 4500' }
+      let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+      it 'knows a 008 length is wrong' do
+        expect(MarcCleanup.bad_008?(record)).to eq true
+      end
+  end
   end
   describe 'fix_008' do
     describe 'fix_book_008' do
