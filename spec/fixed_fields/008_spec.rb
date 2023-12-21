9ce9c0c23076cc0800fcf672ff1841468546b275
Beck Davis <bd4538@princeton.edu>
2023-12-21 14:47:29 -0500

    change computer format 008

--- spec/fixed_fields/008_spec.rb
+++ spec/fixed_fields/008_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'field 008 methods' do
     describe 'bad computer format 008' do
       let(:fields) do
         [
-          { '008' => '210304s2021    cau           000 0 eng z' }
+          { '008' => '230519s1996||||nju|||||||||||||||||eng|z' }
         ]
       end
       let(:leader) { '01104nma a2200289 i 4500' }
