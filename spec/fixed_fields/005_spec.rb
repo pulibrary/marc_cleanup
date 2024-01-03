1e6d67c13ebb005e1ac065179f2ac884c0b4beb5
Beck Davis <136358877+Beck-Davis@users.noreply.github.com>
2024-01-03 09:55:30 -0500

    new tests for the bad_005? method (#92)

---
+++ spec/fixed_fields/005_spec.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'marc'
+require 'byebug'
+require 'marc_cleanup'
+
+RSpec.describe 'bad_005?' do
+
+  context 'when an 005 field is valid' do
+    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+    let(:leader) { '01104naa a2200289 i 4500' }
+    let(:fields) { [ { '005' => '19940223151047.0' } ] }
+  
+    it { expect(MarcCleanup.bad_005?(record)).to eq false }
+  end
+
+  context 'when an 005 is not valid' do
+    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+    let(:leader) { '01104naa a2200289 i 4500' }
+    let(:fields) { [ { '005' => '199402231510470' } ] }
+  
+    it { expect(MarcCleanup.bad_005?(record)).to eq true }
+  end
+  
+  context 'when an 005 is not present' do
+    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+    let(:leader) { '01104naa a2200289 i 4500' }
+    let(:fields) { [ { '001' => '199402231510470' } ] }
+
+    it { expect(MarcCleanup.bad_005?(record)).to eq false }
+  end
+end
