fb7638d2bf980ec4039dd3142b61af07bc887fc4
Mark Zelesky <mzelesky@princeton.edu>
2024-01-03 13:02:14 -0500

    sparse_record for serial language material

--- spec/record_level/sparse_record_spec.rb
+++ spec/record_level/sparse_record_spec.rb
@@ -166,4 +166,74 @@ RSpec.describe 'sparse_record?' do
       it { expect(MarcCleanup.sparse_record?(record)).to eq true }
     end
   end
+  describe 'serial language material' do
+    let(:leader) { '01104nas a2200289 i 4500' }
+    context 'when there is no publication information' do
+      let(:fields) do
+        [
+          { '008' => '230414c19999999xx ||||| |||||||| ||eng||' },
+          { '245' => { 'indicator1' => '0',
+                       'indicator2' => '0',
+                       'subfields' => [{ 'a' => 'Title' }] } }
+        ]
+      end
+      it { expect(MarcCleanup.sparse_record?(record)).to eq true }
+    end
+    context 'when there is a 264b' do
+      let(:fields) do
+        [
+          { '008' => '230414s9999    xx ||||| |||||||| ||eng||' },
+          { '264' => { 'indicator1' => ' ',
+                       'indicator2' => '1',
+                       'subfields' => [{ 'b' => 'Springer' }] } },
+          { '245' => { 'indicator1' => '0',
+                       'indicator2' => '0',
+                       'subfields' => [{ 'a' => 'Title' }] } }
+        ]
+      end
+      it { expect(MarcCleanup.sparse_record?(record)).to eq false }
+    end
+    context 'when there is 260b' do
+      let(:fields) do
+        [
+          { '008' => '230414s9999    xx ||||| |||||||| ||eng||' },
+          { '260' => { 'indicator1' => ' ',
+                       'indicator2' => ' ',
+                       'subfields' => [{ 'b' => 'Springer' }] } },
+          { '245' => { 'indicator1' => '0',
+                       'indicator2' => '0',
+                       'subfields' => [{ 'a' => 'Title' }] } }
+        ]
+      end
+      it { expect(MarcCleanup.sparse_record?(record)).to eq false }
+    end
+    context 'when there is a 533c' do
+      let(:fields) do
+        [
+          { '008' => '230414s9999    xx ||||| |||||||| ||eng||' },
+          { '533' => { 'indicator1' => ' ',
+                       'indicator2' => ' ',
+                       'subfields' => [{ 'c' => 'Acme Corp.' }] } },
+          { '245' => { 'indicator1' => '0',
+                       'indicator2' => '0',
+                       'subfields' => [{ 'a' => 'Title' }] } }
+        ]
+      end
+      it { expect(MarcCleanup.sparse_record?(record)).to eq false }
+    end
+    context 'when there is a 264 field with no subfield b' do
+      let(:fields) do
+        [
+          { '008' => '230414s9999    xx ||||| |||||||| ||eng||' },
+          { '264' => { 'indicator1' => '1',
+                       'indicator2' => ' ',
+                       'subfields' => [{ 'a' => 'New York' }] } },
+          { '245' => { 'indicator1' => '0',
+                       'indicator2' => '0',
+                       'subfields' => [{ 'a' => 'Title' }] } }
+        ]
+      end
+      it { expect(MarcCleanup.sparse_record?(record)).to eq true }
+    end
+  end
 end
