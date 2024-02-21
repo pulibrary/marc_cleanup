996426805d3fd01296ee990bca8bf9b938f63cb0
Beck Davis <bd4538@princeton.edu>
2024-02-21 09:47:55 -0500

    rda_convention_mismatch method and test

--- spec/record_level/rda_convention_mismatch_spec.rb
+++ spec/record_level/rda_convention_mismatch_spec.rb
@@ -5,7 +5,15 @@ require 'marc_cleanup'
 
 RSpec.describe "rda_convention_mismatch" do
   let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-  let(:leader) { '01104naa a2200289 i 4500' }
-  let(:fields) { [ { '040' =>  { 'subfields' => [{ 'e' => 'aarc2' }] } } ] }
-  it { expect(MarcCleanup.rda_convention_mismatch(record)).to eq true }
+  let(:leader) { '01104naa a2200289 a 4500' }
+  let(:fields) do
+      [ 
+        { '040' => { 'indicator1' => ' ',
+                     'indicator2' => ' ',
+                     'subfields' => [{ 'e' => 'rda' }] } }
+      ]
+  end
+  it "finds mismatches" do
+    expect(MarcCleanup.rda_convention_mismatch(record)).to eq true
+  end
 end
