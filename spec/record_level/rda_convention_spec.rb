f56372ae19db317813d4c5f07480f36eaeefd564
Beck Davis <136358877+Beck-Davis@users.noreply.github.com>
2024-02-22 09:17:12 -0500

    Update rda_convention_spec.rb

--- spec/record_level/rda_convention_spec.rb
+++ spec/record_level/rda_convention_spec.rb
@@ -4,20 +4,20 @@ require 'byebug'
 require 'marc_cleanup'
 
 RSpec.describe "rda description convention relationship" do
-  # describe "rda_convention_mismatch" do
-  #   let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
-  #   let(:leader) { '01104naa a2200289 a 4500' }
-  #   let(:fields) do
-  #       [ 
-  #         { '040' => { 'indicator1' => ' ',
-  #                     'indicator2' => ' ',
-  #                     'subfields' => [{ 'e' => 'rda' }] } }
-  #       ]
-  #   end
-  #   it "finds mismatches" do
-  #     expect(MarcCleanup.rda_convention_mismatch(record)).to eq true
-  #   end
-  # end
+  describe "rda_convention_mismatch" do
+    let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
+    let(:leader) { '01104naa a2200289 a 4500' }
+    let(:fields) do
+        [ 
+          { '040' => { 'indicator1' => ' ',
+                      'indicator2' => ' ',
+                      'subfields' => [{ 'e' => 'rda' }] } }
+        ]
+    end
+    it "finds mismatches" do
+      expect(MarcCleanup.rda_convention_mismatch(record)).to eq true
+    end
+  end
   
   describe "rda_convention_correction" do
     let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
