825d5cafc057c278a4c3cd2b8bf4aa4e21edd59c
tventimi <tventimi@princeton.edu>
2025-02-03 11:12:27 -0500

    rubocop replace_field_spec
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>

--- spec/record_level/replace_field_spec.rb
+++ spec/record_level/replace_field_spec.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'marc'
 require 'byebug'
 require 'marc_cleanup'
 
 RSpec.describe 'replace_field' do
+  let(:record) { MARC::Record.new_from_hash('fields' => fields) }
   context 'source/target both fixed fields' do
-    let(:fields) { [ { '009' => 'Tom' } ] }
-    let(:record) { MARC::Record.new_from_hash('fields' => fields) }
+    let(:fields) { [{ '009' => 'Tom' }] }
     let(:field_string) { '009 Tom' }
     let(:replacement_field) { MARC::ControlField.new('009', 'Mark') }
     it 'replaces content' do
@@ -19,16 +21,15 @@ RSpec.describe 'replace_field' do
       [
         { '020' => { 'indicator1' => ' ',
                      'indicator2' => ' ',
-                     'subfields' => [{ 'a' => '9780316458759' }] } },
+                     'subfields' => [{ 'a' => '9780316458759' }] } }
       ]
     end
-    let(:record) { MARC::Record.new_from_hash('fields' => fields) }
     let(:field_string) { '020    $a 9780316458759' }
     let(:replacement_field) { MARC::DataField.new('500', '1', '0', MARC::Subfield.new('a', 'Tom and Mark')) }
     it 'replaces content' do
       replace_field(field_string: field_string, replacement_field: replacement_field, record: record)
       expect(record['020']).to be_nil
-      expect(record['500']['a']).to eq "Tom and Mark"
+      expect(record['500']['a']).to eq 'Tom and Mark'
     end
   end
 end
