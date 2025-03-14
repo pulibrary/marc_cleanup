1ced5681e701e378705dcb5d300337bb3354329b
mzelesky <mzelesky@princeton.edu>
2025-03-14 16:16:55 -0400

    refactor extra_spaces and extra_space_fix (#165)
    * refactor extra_spaces and extra_space_fix
    
    * add additional test for extra_space_fix

--- spec/variable_fields/extra_spaces_spec.rb
+++ spec/variable_fields/extra_spaces_spec.rb
@@ -6,12 +6,28 @@ RSpec.describe 'extra_spaces?' do
   let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
   let(:leader) { '01104naa a2200289 i 4500' }
 
-  context '1xx field has extra spaces' do
+  context '100 field has extra spaces' do
     let(:fields) do
       [
         { '100' => { 'ind1' => '0',
                      'ind2' => ' ',
                      'subfields' => [{ 'a' => 'Candy  wrapper.' }] } }
+      ]
+    end
+    it 'triggers an error' do
+      expect(MarcCleanup.extra_spaces?(record)).to eq true
+    end
+  end
+
+  context '880 field linked to a 130 field has extra spaces' do
+    let(:fields) do
+      [
+        { '880' => { 'ind1' => '0',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { '6' => '130-00' },
+                       { 'a' => 'Καραμέλα ' }
+                     ] } }
       ]
     end
     it 'triggers an error' do
@@ -25,12 +41,45 @@ RSpec.describe 'extra_spaces?' do
         { '533' => { 'ind1' => ' ',
                      'ind2' => ' ',
                      'subfields' => [
-                                      { 'a' => "Positive\u3000photo" },
-                                      { '7' => 's1989    nyu  a'}] } }
+                       { 'a' => "Positive\u3000photo" },
+                       { '7' => 's1989    nyu  a' }
+                     ] } }
       ]
     end
     it 'does not triggers an error' do
       expect(MarcCleanup.extra_spaces?(record)).to eq false
+    end
+  end
+
+  context '880 field linked to a 533 field has extra spaces' do
+    let(:fields) do
+      [
+        { '880' => { 'ind1' => ' ',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { '6' => '533-00' },
+                       { 'a' => ' Μικροφίλμ' }
+                     ] } }
+      ]
+    end
+    it 'triggers an error' do
+      expect(MarcCleanup.extra_spaces?(record)).to eq true
+    end
+  end
+
+  context '880 field linked to an 830 field has extra spaces' do
+    let(:fields) do
+      [
+        { '880' => { 'ind1' => ' ',
+                     'ind2' => '0',
+                     'subfields' => [
+                       { '6' => '830-00' },
+                       { 'a' => ' Μικροφίλμ' }
+                     ] } }
+      ]
+    end
+    it 'triggers an error' do
+      expect(MarcCleanup.extra_spaces?(record)).to eq true
     end
   end
 
@@ -39,7 +88,8 @@ RSpec.describe 'extra_spaces?' do
       [
         { '775' => { 'ind1' => '0',
                      'ind2' => ' ',
-                     'subfields' => [ { 'a' => 'Modernist   thought' }] } }]
+                     'subfields' => [{ 'a' => 'Modernist   thought' }] } }
+      ]
     end
     it 'triggers an error' do
       expect(MarcCleanup.extra_spaces?(record)).to eq true
@@ -51,7 +101,8 @@ RSpec.describe 'extra_spaces?' do
       [
         { '830' => { 'ind1' => ' ',
                      'ind2' => '4',
-                     'subfields' => [ { 'a' => 'The modern world.' }] } }]
+                     'subfields' => [{ 'a' => 'The modern world.' }] } }
+      ]
     end
     it 'does not trigger an error' do
       expect(MarcCleanup.extra_spaces?(record)).to eq false
@@ -82,12 +133,29 @@ RSpec.describe 'extra_space_fix' do
         { '533' => { 'ind1' => ' ',
                      'ind2' => ' ',
                      'subfields' => [
-                                      { 'a' => "Positive\u3000photo" },
-                                      { '7' => 's1989    nyu  a'}] } }
+                       { 'a' => "Positive\u3000photo" },
+                       { '7' => 's1989    nyu  a' }
+                     ] } }
       ]
     end
     it 'does not modify the positionally-defined subfield' do
       expect(MarcCleanup.extra_space_fix(record)['533']['7']).to eq 's1989    nyu  a'
+    end
+  end
+
+  context '880 field linked to a 533 field has extra spaces' do
+    let(:fields) do
+      [
+        { '880' => { 'ind1' => ' ',
+                     'ind2' => ' ',
+                     'subfields' => [
+                       { '6' => '533-00' },
+                       { 'a' => ' Μικροφίλμ' }
+                     ] } }
+      ]
+    end
+    it 'removes the extra spaces' do
+      expect(MarcCleanup.extra_space_fix(record)['880']['a']).to eq 'Μικροφίλμ'
     end
   end
 
@@ -96,7 +164,8 @@ RSpec.describe 'extra_space_fix' do
       [
         { '775' => { 'ind1' => '0',
                      'ind2' => ' ',
-                     'subfields' => [ { 'a' => 'Modernist   thought ' }] } }]
+                     'subfields' => [{ 'a' => 'Modernist   thought ' }] } }
+      ]
     end
     it 'removes the extra spaces' do
       expect(MarcCleanup.extra_space_fix(record)['775']['a']).to eq 'Modernist thought'
@@ -108,7 +177,8 @@ RSpec.describe 'extra_space_fix' do
       [
         { '830' => { 'ind1' => ' ',
                      'ind2' => '4',
-                     'subfields' => [ { 'a' => 'The modern world.' }] } }]
+                     'subfields' => [{ 'a' => 'The modern world.' }] } }
+      ]
     end
     it 'does not modify the record' do
       expect(MarcCleanup.extra_space_fix(record)['830']['a']).to eq 'The modern world.'
