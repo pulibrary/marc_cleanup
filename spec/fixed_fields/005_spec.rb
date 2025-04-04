2eb628ca27f4f17ff57ae47be1d2e0f6e676f145
Mark Zelesky <mzelesky@princeton.edu>
2025-04-04 12:14:23 -0400

    add frozen_string_literal to fixed_fields and rubocop autofixes on fixed_fields

--- spec/fixed_fields/005_spec.rb
+++ spec/fixed_fields/005_spec.rb
@@ -3,14 +3,14 @@ require 'marc'
 require 'byebug'
 require 'marc_cleanup'
 
-RSpec.describe 'bad_005?' do
+RSpec.describe 'bad_f005?' do
 
   context 'when an 005 field is valid' do
     let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
     let(:leader) { '01104naa a2200289 i 4500' }
     let(:fields) { [ { '005' => '19940223151047.0' } ] }
-  
-    it { expect(MarcCleanup.bad_005?(record)).to eq false }
+
+    it { expect(MarcCleanup.bad_f005?(record)).to eq false }
   end
 
   context 'when an 005 is not valid' do
@@ -18,14 +18,14 @@ RSpec.describe 'bad_005?' do
     let(:leader) { '01104naa a2200289 i 4500' }
     let(:fields) { [ { '005' => '199402231510470' } ] }
   
-    it { expect(MarcCleanup.bad_005?(record)).to eq true }
+    it { expect(MarcCleanup.bad_f005?(record)).to eq true }
   end
-  
+
   context 'when an 005 is not present' do
     let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
     let(:leader) { '01104naa a2200289 i 4500' }
     let(:fields) { [ { '001' => '199402231510470' } ] }
 
-    it { expect(MarcCleanup.bad_005?(record)).to eq false }
+    it { expect(MarcCleanup.bad_f005?(record)).to eq false }
   end
 end
