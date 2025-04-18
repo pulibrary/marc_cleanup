7fbc842b816c4a9afefe234516c75099a58667eb
Mark Zelesky <mzelesky@princeton.edu>
2025-04-18 09:45:44 -0400

    refactoring record_level

--- spec/record_level/field_count_spec.rb
+++ spec/record_level/field_count_spec.rb
@@ -1,26 +1,41 @@
+# frozen_string_literal: true
+
 require 'marc_cleanup'
 require 'byebug'
 
 RSpec.describe 'field_count' do
-  describe 'get field count' do
-    let(:fields) do
-      [
-        { '001' => '1234' },
-        { '100' => { 'ind1' => ' ',
-                     'ind2' => ' ',
-                     'subfields' => [{ 'a' => 'field 1a' }] } },
-        { '130' => { 'ind1' => ' ',
-                     'ind2' => ' ',
-                     'subfields' => [{ 'a' => 'field 2a' }, { 'd' => 'field 2d' }, { 'd' => 'field 2d2' }] } }
+  let(:fields) do
+    [
+      { '001' => '1234' },
+      { '100' => { 'ind1' => ' ',
+                   'ind2' => ' ',
+                   'subfields' => [{ 'a' => 'field 1a' }] } },
+      { "13\xc3\x30" => { 'ind1' => ' ',
+                          'ind2' => ' ',
+                          'subfields' => [
+                            { 'a' => 'field 2a' },
+                            { 'd' => 'field 2d' },
+                            { 'd' => 'field 2d2' }
+                          ] } }
 
-      ]
-    end
-    let(:record) { MARC::Record.new_from_hash('fields' => fields) }
+    ]
+  end
+  let(:record) { MARC::Record.new_from_hash('fields' => fields) }
+
+  context 'without subfields option' do
     it 'reports field count' do
-      expect(MarcCleanup.field_count(record).values.sum).to eq 3
+      count = MarcCleanup.field_count(record)
+      expect(count.values.sum).to eq 3
+      expect(count['130']).to eq 1
     end
-    it 'reports fixed field count plus subfield count' do
-      expect(MarcCleanup.field_count(record, subfields: true).values.sum).to eq 5
+  end
+
+  context 'with subfields option' do
+    it 'reports fixed field count plus subfield count of datafields' do
+      count = MarcCleanup.field_count(record, subfields: true)
+      expect(count.values.sum).to eq 5
+      expect(count['130a']).to eq 1
+      expect(count['130d']).to eq 2
     end
   end
 end
