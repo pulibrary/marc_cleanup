446af8ce5df3ea4834a16e5643c9c1524f731d7b
mzelesky <mzelesky@princeton.edu>
2026-01-13 12:24:19 -0500

    yaml files for fixed field codes and rubocop autocorrect (#198)

--- spec/fixed_fields/006_spec.rb
+++ spec/fixed_fields/006_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'marc'
 require 'byebug'
@@ -7,18 +9,17 @@ RSpec.describe 'field 006 methods' do
   let(:record) { MARC::Record.new_from_hash('fields' => fields, 'leader' => leader) }
 
   describe 'bad_f006?' do
-
     describe 'book format' do
       let(:leader) { '01104naa a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'a    fsf   |001 0 ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'a    fsf   |001 0 ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'a    fsf   |001 3 ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'a    fsf   |001 3 ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -26,13 +27,13 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104nma a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'm    fo  b |      ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'm    fo  b |      ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'm    fo  b |     x' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'm    fo  b |     x' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -40,13 +41,13 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104nea a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'eb   aa a  aa 0   ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'eb   aa a  aa 0   ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'eb   aa a  aa l   ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'eb   aa a  aa l   ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -54,13 +55,13 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104nca a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'canadaaa     a  a ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'canadaaa     a  a ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'canadaaa     a  l ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'canadaaa     a  l ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -68,13 +69,13 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104nab a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 's | |||||||a0    0' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 's | |||||||a0    0' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 's | |||||||a0    3' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 's | |||||||a0    3' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -82,13 +83,13 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104nga a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'g135 f     fs   ma' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'g135 f     fs   ma' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'g135 f     fs   mb' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'g135 f     fs   mb' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
 
@@ -96,19 +97,18 @@ RSpec.describe 'field 006 methods' do
       let(:leader) { '01104npa a2200289 i 4500' }
 
       context 'when the 006 field is valid' do
-        let(:fields) { [ { '006' => 'p     a           ' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq false }
+        let(:fields) { [{ '006' => 'p     a           ' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq false }
       end
 
       context 'when the 006 field is invalid' do
-        let(:fields) { [ { '006' => 'p     a          9' } ] }
-        it {expect(MarcCleanup.bad_f006?(record)).to eq true }
+        let(:fields) { [{ '006' => 'p     a          9' }] }
+        it { expect(MarcCleanup.bad_f006?(record)).to eq true }
       end
     end
   end
 
   describe 'fix_f006' do
-
     describe 'fix book format 006' do
       let(:leader) { '01104naa a2200289 i 4500' }
       let(:fields) do
@@ -158,7 +158,7 @@ RSpec.describe 'field 006 methods' do
       let(:fields) do
         [
           { '006' => 's | |||||||n0    |' },
-          { '008' => '230519s1996    njua          0   0 eng d'}
+          { '008' => '230519s1996    njua          0   0 eng d' }
         ]
       end
       it { expect(fix_f006(record)['006'].value).to eq 's | |||||||o0    |' }
